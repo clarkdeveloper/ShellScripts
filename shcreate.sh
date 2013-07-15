dc5936551211665dcef8a692abbe7c24006207c1
Clark Burns <clark@castironcoding.com>
2013-07-15 11:21:45 -0700

    [CHORE] Removed version identifier when new script is created

--- shcreate.sh
+++ shcreate.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 #Author: Clark - clark@lotdf.com
-#Ver. 0.1.0
 #Purpose: To be as lazy as possible when creating other scripts to accomplish tasks without too much effort.  
 
 newsh="${1}.sh"
@@ -9,7 +8,7 @@ echo "Creating file: ${newsh}"
 
 touch ${newsh}
 
-echo "#!/bin/bash\n#Author: Clark - clark@lotdf.com\n#Ver. 0.1.0\n#Purpose:\n\n\n\necho \"All done.\"\nexit 0" >> ${newsh}
+echo "#!/bin/bash\n#Author: Clark - clark@lotdf.com\n#Purpose:\n\n\n\necho \"All done.\"\nexit 0" >> ${newsh}
 
 echo "Done."
 
